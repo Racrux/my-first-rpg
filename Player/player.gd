542ed7fcac52e51ec527f64b6d633e1e982d8c1b
Racrux <yjracoon@gmail.com>
2024-09-04 22:13:22 +0900

    Update player.gd
    implemented acceleration and velocity to make the character move smoothly without jittering.

--- Player/player.gd
+++ Player/player.gd
@@ -1,16 +1,23 @@
 extends CharacterBody2D
 
-
-
+# multiply delta to make it frame-independent (real-world time)
+const ACCELERATION = 50
+const MAX_SPEED = 200
+const FRICTION = 400
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
- 
+	# nomalize the vector values to make speed consistent in diff angles
+	input_vector = input_vector.normalized()
+
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity += input_vector * ACCELERATION * delta
+		# IMPORTANT: Godot v4 has limit_length() instead of clamped()
+		# Reference: https://forum.godotengine.org/t/clamped-doesnt-work-godot-4-0/1284/6
+		velocity = velocity.limit_length(MAX_SPEED * delta)
 	else:
-		velocity = Vector2.ZERO
-		
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+
 	move_and_collide(velocity)
