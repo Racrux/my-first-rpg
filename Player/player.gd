8c190c9cfb628575e12b1fb9760dbae959c034e1
Racrux <yjracoon@gmail.com>
2024-09-08 00:10:07 +0900

    implemented character animation
    - currently only left and right animations were implmented

--- Player/player.gd
+++ Player/player.gd
@@ -5,6 +5,10 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+# GODOTv4 uses @annotation 
+# Reference: https://forum.godotengine.org/t/onready-unexpected-identifier/10532/2
+@onready var animationPlayer = $AnimationPlayer
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -13,9 +17,15 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
+
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	
 	else:
+		animationPlayer.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	# move_and_slide automatically applies delta
