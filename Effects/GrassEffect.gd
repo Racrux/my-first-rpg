dd79ce9cb181d34d84e17c1424866abbe688e5fc
Racrux <yjracoon@gmail.com>
2024-09-15 02:27:01 +0900

    fixed grass destroy animation
    - enabled auto play on load for animation
      - fixes animation repetition

--- Effects/GrassEffect.gd
+++ Effects/GrassEffect.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 @onready var animatedSprite = $AnimatedSprite
 
-func _process(delta):
+func ready():
+	animatedSprite.frame = 0
 	animatedSprite.play("Animate")
-
 
 func _on_animated_sprite_2d_animation_finished():
 	queue_free()
