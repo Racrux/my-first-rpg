ee1fccefbe1e26500e5077d29c095df99fa84f36
Racrux <yjracoon@gmail.com>
2024-09-15 20:16:12 +0900

    Updated code based on part 12
    - implemented roll state
     - added another BlendSpace2D in animationTree for Roll
      - reduced sliding

--- Player/player.gd
+++ Player/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 # multiply delta to make it frame-independent (real-world time)
 const ACCELERATION = 500
 const MAX_SPEED = 80
+# different from one on video
+# suggested 120, somehow too fast on mine
+const ROLL_SPEED = 1.5
 const FRICTION = 500
 
 enum{
@@ -12,7 +15,8 @@ enum{
 }
 
 var state = MOVE
-
+# sets roll vector while attempting to move, if input vector = zero --> no roll
+var roll_vector = Vector2.LEFT
 
 # GODOTv4 uses @annotation 
 # Reference: https://forum.godotengine.org/t/onready-unexpected-identifier/10532/2
@@ -32,7 +36,7 @@ func _physics_process(delta):
 			move_state(delta)
 
 		ROLL: 
-			pass
+			roll_state(delta)
 
 		ATTACK:
 			attack_state(delta)
@@ -45,6 +49,7 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		# set blend position for idle/run
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
@@ -52,6 +57,7 @@ func move_state(delta):
 		#  - Input vector isnt inside attack state
 		#  - Don't want player to change direction during animation
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
@@ -59,19 +65,37 @@ func move_state(delta):
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	move()
+	
 
-	# move_and_slide automatically applies delta
-	# Godot v4 also automatically uses velocity defined unlike Godot v3.2
-	move_and_slide()
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
 
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
+
+func roll_state(delta):
+	# no acceleration
+	velocity = roll_vector * MAX_SPEED * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
+
+func roll_animation_finished():
+	# Reduce sliding
+	velocity = velocity * 0.8
+	state = MOVE
 
 func attack_state(delta):
 	# no velocity during motion
 	velocity = Vector2.ZERO
 	
 	animationState.travel("Attack")
+
+func move():
+	# move_and_slide automatically applies delta
+	# Godot v4 also automatically uses velocity defined unlike Godot v3.2
+	move_and_slide()
 
 func attack_animation_finished():
 	state = MOVE
