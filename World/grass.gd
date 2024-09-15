e7743f2514157a28e1eb48fe8c54be98ec4a8eef
Racrux <yjracoon@gmail.com>
2024-09-15 19:48:25 +0900

    Updated code based on part 11
    - implemented hitbox / hurtbox
     - sword hits grass --> grass destroys

--- World/grass.gd
+++ World/grass.gd
@@ -1,20 +1,20 @@
 extends Node2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		# Scene
-		var GrassEffect = load("res://Effects/grass_effect.tscn")
-		# Node
-		# In GODOT v4, use "instantiate()" instead of "instance()"
-		var grassEffect = GrassEffect.instantiate()
+func create_grass_effect():
+	var GrassEffect = load("res://Effects/grass_effect.tscn")
+	# Node
+	# In GODOT v4, use "instantiate()" instead of "instance()"
+	var grassEffect = GrassEffect.instantiate()
 		
-		# Gets current main scene of scene tree (remote, NOT local)
-		var world = get_tree().current_scene
-		# add the instance of GrassEffect scene
-		world.add_child(grassEffect)
-		# set grass effect's global position to global position of the grass
+	# Gets current main scene of scene tree (remote, NOT local)
+	var world = get_tree().current_scene
+	# add the instance of GrassEffect scene
+	world.add_child(grassEffect)
+	# set grass effect's global position to global position of the grass
 		
-		# REMEBER TO ATTACH SCRIPT TO EACH AFFECTED NODES!!!
-		grassEffect.global_position = global_position
-		queue_free()
+	# REMEBER TO ATTACH SCRIPT TO EACH AFFECTED NODES!!!
+	grassEffect.global_position = global_position
+
+func _on_hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
