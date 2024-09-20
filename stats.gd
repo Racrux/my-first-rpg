8f3c3b89a1e1c9290142b21d00f4d8efe6804ff9
Racrux <yjracoon@gmail.com>
2024-09-20 23:39:09 +0900

    Updated code based on P18
    - added the Player Stat UI
    - refactored the setters and getters in stats
    - added shadows to entities

--- stats.gd
+++ stats.gd
@@ -1,13 +1,25 @@
 extends Node
 
-@export var max_health: int = 4
-@onready var health = max_health:
-	get: 
-		return health
-	set(value): 
+@export var max_health: int = 4:set = set_max_health
+var health: int = max_health:set = set_health
+
+signal no_health
+signal health_changed(value)
+signal max_health_changed(value)
+
+
+func set_max_health(value):
+	max_health = value
+	# health can never be larger than max_health
+	self.health = min(health, max_health)
+	emit_signal("max_health_changed", max_health)
+
+
+func set_health(value): 
 		health = value
+		emit_signal("health_changed", health)
 		if health <= 0:
 			emit_signal("no_health")
 			emit_signal("no_health")
-
-signal no_health
+func _ready():
+	self.health = max_health
